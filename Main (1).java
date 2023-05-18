e6ef1b34c8f79e20d3944d211fb3307e615ba787
tavisha2 <104326277+tavisha2@users.noreply.github.com>
2023-05-18 10:06:48 -0700

    Update Main (1).java

--- Main (1).java
+++ Main (1).java
@@ -1,10 +1,42 @@
+package fls;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.*;
 class Main {
   public static void main(String[] args) {
     Frame f = new Frame();
-    f.setSize(600,500); 
+    f.addWindowListener(new WindowAdapter() {
+    	public void windowClosing(WindowEvent e) {
+    		System.exit(0);
+    	}
+    });
+    f.setSize(600,700); 
     f.setBackground(new Color(0,0,0));
+    
+    NameGetter n = new NameGetter(1);
+    while (n.getName() == null){
+    	n.setVisible(true);
+    }
+    Player p = new Player(n.getName(), 30);
+    f.add(p);
+    
+    NameGetter n2 = new NameGetter(2);
+    while (n2.getName() == null){
+    	n2.setVisible(true);
+    }
+    Player p2 = new Player(n2.getName(), 150);
+    f.add(p2);
+    
+    n.setVisible(false);
+    n.setLocationRelativeTo(f);
+    
+    Player current = p;
+    
     for(int i = 0; i < 6;i++){
       Title t = new Title("replace",20 + (90*i),100);
       f.add(t);
@@ -14,6 +46,11 @@ class Main {
       int y = 110;
       for (int j = 200; j <= 1000; j+=200){
         Card c = new Card(x,50 + y,""+ j);
+        c.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent e) {
+        		//do something
+        	}
+        });
         y+=60;
         f.add(c);
       }
@@ -21,11 +58,7 @@ class Main {
     }
 
     
-    NameGetter n = new NameGetter();
     
-    Player p = new Player("Bob",30);
-    Player p2 = new Player("Bob2",90);
-    f.add(p);
 
     
 
