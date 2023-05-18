94d96f1df8a1da53b952d968476644e2d157bc1c
tavisha2 <104326277+tavisha2@users.noreply.github.com>
2023-05-18 10:07:43 -0700

    Update NameGetter.java

--- NameGetter.java
+++ NameGetter.java
@@ -1,22 +1,37 @@
+package fls;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 public class NameGetter extends JDialog{
-  public NameGetter(){
+	private String name;
+  public NameGetter(int num){
     this.setBounds(200,200,250,200);
     this.setBackground(new Color(0,0,170));
-    JButton b = new JButton("Submit");
-    b.setBounds(70,130,100,30);
-    b.setBackground(new Color(0,0,255));
-    b.setForeground(new Color(255,255,0));
-    this.add(b);
     TextField t = new TextField("", 90);
     t.setBounds(50,80,150,20);
     this.add(t);
-    JLabel l = new JLabel("Please enter player 1's name: ");
-    l.setBounds(40,30,150,20);
+    JLabel l = new JLabel("Please enter player" + num + "'s name: ");
+    l.setBounds(30,30,190,20);
     l.setForeground(new Color(0,0,0));
     this.add(l);
+    JButton b = new JButton("Submit");
+    b.setBounds(70,110,100,30);
+    b.setBackground(new Color(0,0,255));
+    b.setForeground(new Color(255,255,0));
+   
+    b.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent e) {
+    		name = t.getText();
+    	}
+    });
+    this.add(b);
     this.setLayout(null);
     this.setVisible(true);
+  }
+  
+  public String getName() {
+	  return name;
   }
 }
