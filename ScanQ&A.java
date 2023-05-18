b8fc60f5b4f95c293f1e09ed64a2c44e2c315c87
Aubrilee <96450723+Aubrilee@users.noreply.github.com>
2023-05-18 13:36:41 -0700

    Update ScanQ&A.java

--- ScanQ&A.java
+++ ScanQ&A.java
@@ -1,7 +1,7 @@
 import java.awt.geom.Path2D;
 import java.io.File;
 import java.util.*;
-/*
+
 public class ScanQA {
   public static void ScanQA(){
     private String question;
@@ -15,10 +15,11 @@ public class ScanQA {
   }
 
   public static getName {
-  String qndA [5][5] = {{"C1", "C2", "C3", "C4", "C5"},{"Q1 A1", "Q2 A2", "Q3 A3", "Q4 A4", "Q5 A5"}, {"Q6 A6", "Q7 A7", "Q8 A8", "Q9 A9", "Q10 A10"},{"Q11 A11", "Q12 A12", "Q13 A13","Q14 A14", "Q15 A15"}, {"Q16 A16", "Q17 A17", "Q18 A18", "Q19 A19", "Q20 A20"}};
+    String categoryQuestions [5][5] = {{"C1", "C2", "C3", "C4", "C5"},{"Q1 A1", "Q2 A2", "Q3 A3", "Q4 A4", "Q5 A5"}, {"Q6 A6", "Q7 A7", "Q8 A8", "Q9 A9", "Q10 A10"},{"Q11 A11", "Q12 A12", "Q13 A13","Q14 A14", "Q15 A15"}, {"Q16 A16", "Q17 A17", "Q18 A18", "Q19 A19", "Q20 A20"}};
+    String answers [4][4] = {{"A1", "A2", "A3", "A4", "A5"}, {"A6", "A7", "A8", "A9", "A10"},{"A11", "A12", "A13","A14", "A15"}, {"A16", "A17", "A18", "A19", "A20"}};
   
   }
   
 }
 
-*/
+
