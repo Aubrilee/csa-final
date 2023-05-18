2cabacb44bd1cf81b2a72ce9ca6b421f13cb3c57
Aubrilee <96450723+Aubrilee@users.noreply.github.com>
2023-05-18 09:51:40 -0700

    Update ScanQ&A.java

--- ScanQ&A.java
+++ ScanQ&A.java
@@ -14,7 +14,10 @@ public class ScanQA {
     
   }
 
-  public static 
+  public static getName {
+  String qndA [5][5] = {{"C1", "C2", "C3", "C4", "C5"},{"Q1 A1", "Q2 A2", "Q3 A3", "Q4 A4", "Q5 A5"}, {"Q6 A6", "Q7 A7", "Q8 A8", "Q9 A9", "Q10 A10"},{"Q11 A11", "Q12 A12", "Q13 A13","Q14 A14", "Q15 A15"}, {"Q16 A16", "Q17 A17", "Q18 A18", "Q19 A19", "Q20 A20"}};
+  
+  }
   
 }
 
