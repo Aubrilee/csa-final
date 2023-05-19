03ea7f34ea43577f3eb3cc9e31ef859751a103c8
Aubrilee <96450723+Aubrilee@users.noreply.github.com>
2023-05-19 07:54:46 -0700

    Update ScanQ&A.java

--- ScanQ&A.java
+++ ScanQ&A.java
@@ -1,17 +1,45 @@
 import java.awt.geom.Path2D;
-import java.io.File;
+//import java.io.File;
 import java.util.*;
 
 public class ScanQA {
+  public static void HardC(){
+    String c1 = "History";
+    String c2 = "ESPN's top 10 All Time";
+    String c3 = "Epitaphs & Tributes";
+    String c4 = "3-Letter Words";
+    
+    String[][] questions = new String[5][4];
+    String[][] answers = new String[5][4];
+    
+    Scanner scanQ = new Scanner();
+    Scanner scanA = new Scanner();
+    
+    for (int i = 0; i < 5; i++){
+      for (int j = 0; j < 4; j++){
+        questions[i][j] = scanQ.nextLine();
+      }
+    }
+      
+    for (int i = 0; i < 5; i++){
+      for (int j = 0; j < 4; j++){
+        answers[i][j] = scanA.nextLine();
+      }
+    }
+  }
+  
+/*
+  private String question;
+  private String answer;
+  private String category;
+  private String amount;
   public static void ScanQA(){
-    private String question;
-    private String answer;
-    private String category;
-    private String amount;
-
+  */
+    
+/*
     File qaSheet = new File("Q and A Table");
     Scanner s = new Scanner(qaSheet);
-    
+ 
   }
 
   public static getName {
@@ -19,7 +47,7 @@ public class ScanQA {
     String answers [4][4] = {{"A1", "A2", "A3", "A4", "A5"}, {"A6", "A7", "A8", "A9", "A10"},{"A11", "A12", "A13","A14", "A15"}, {"A16", "A17", "A18", "A19", "A20"}};
   
   }
-  
+  */
 }
 
 
