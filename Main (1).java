b6e93e0a8173ae5597bb5c8aecdc5b48221b9123
tavisha2 <104326277+tavisha2@users.noreply.github.com>
2023-05-18 22:51:09 -0700

    Update Main (1).java

--- Main (1).java
+++ Main (1).java
@@ -6,18 +6,23 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.*;
+import javax.swing.JButton;
+
 class Main {
   public static void main(String[] args) {
+	String[] categories = {"HISTORY","ESPN's TOP 10 ALL-TIME ATHLETES","EVERYBODY TALKS ABOUT IT...","THE COMPANY LINE","EPITAPHS & TRIBUTES","3-LETTER WORDS"};
+	String[][] questions = {{"Q1", "Q2", "Q3","Q4","Q5","Q6"},{"Q1","Q2"}};
+	String[][] answers = {{"A1","A2","A3","A4","A5","A6"},{"A1","A2","A3","A4","A5","A6"}};
+	
     Frame f = new Frame();
     f.addWindowListener(new WindowAdapter() {
     	public void windowClosing(WindowEvent e) {
     		System.exit(0);
     	}
     });
-    f.setSize(600,700); 
+    f.setSize(1000,800); 
     f.setBackground(new Color(0,0,0));
-    
+    /*
     NameGetter n = new NameGetter(1);
     while (n.getName() == null){
     	n.setVisible(true);
@@ -33,34 +38,46 @@ class Main {
     f.add(p2);
     
     n.setVisible(false);
-    n.setLocationRelativeTo(f);
+    n2.setVisible(false);
     
     Player current = p;
-    
+    */
     for(int i = 0; i < 6;i++){
-      Title t = new Title("replace",20 + (90*i),100);
+      Title t = new Title(categories[i],20 + (160*i),100);
       f.add(t);
     }
     int x = 20;
     for (int i = 0; i < 6; i++){
-      int y = 110;
+      int y = 140;
       for (int j = 200; j <= 1000; j+=200){
         Card c = new Card(x,50 + y,""+ j);
         c.addActionListener(new ActionListener(){
         	public void actionPerformed(ActionEvent e) {
-        		//do something
+        		c.setBackground(new Color(100,100,100));
+        		c.setEnabled(false);
         	}
         });
-        y+=60;
+        y+=90;
         f.add(c);
       }
-      x+= 90;
+      x+= 160;
     }
-
     
+    TextField t = new TextField("", 90);
+    t.setBounds(400,700,150,20);
+    f.add(t);
     
-
-    
+    JButton submit = new JButton("Submit");
+    submit.setBounds(575,695,100,30);
+    submit.setBackground(new Color(0,0,255));
+    submit.setForeground(new Color(255,255,0));
+   
+    submit.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent e) {
+    		//smoething
+    	}
+    });
+    f.add(submit);
 
     f.setLayout(null);   
     f.setVisible(true); 
