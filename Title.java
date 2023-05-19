925b4171ac17401991ffdfefea4d4d29b9762b26
tavisha2 <104326277+tavisha2@users.noreply.github.com>
2023-05-18 22:52:08 -0700

    Update Title.java

--- Title.java
+++ Title.java
@@ -1,12 +1,17 @@
+package fls;
 import javax.swing.*;
 import java.awt.*;
 public class Title extends JLabel{
   public Title(String val, int x, int y){
     super(val);
-    this.setBounds(x,y,80,50);
-    this.setBackground(new Color(0,0,255));
+    this.setBounds(x,y,150,80);
+    this.setFont(new Font("Serif",3, 13));
+    if (val.length() > 20) {
+    	this.setFont(new Font("Serif",1,8));
+    }
+    this.setBackground(new Color(255,0,255));
     this.setForeground(new Color(255,255,0));
-    this.setAlignmentX(CENTER_ALIGNMENT);
-    this.setAlignmentY(CENTER_ALIGNMENT);
+    this.setAlignmentX(SwingConstants.CENTER);
+    this.setAlignmentY(SwingConstants.CENTER);
   }
 }
