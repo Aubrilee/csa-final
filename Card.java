255a8fbf060c8e284474684cad5648e7b2eb3d08
tavisha2 <104326277+tavisha2@users.noreply.github.com>
2023-05-21 20:19:14 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,9 +1,19 @@
 package fls;
 import java.awt.*;
 public class Card extends Button{
-  public Card(int x, int y, String val){
+  private int row;
+  private int col;
+  public Card(int x, int y, String val,int row, int col){
     super(val);
+    this.row = row;
+    this.col = col;
     this.setBounds(x,y,	150,80);
     this.setBackground(new Color(0,0,255));
+  }
+  public int getRow() {
+	  return row;
+  }
+  public int getCol() {
+	  return col;
   }
 }
