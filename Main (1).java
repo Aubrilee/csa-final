c6d2746a0aede0c5572525bc5f3f4c9c666eed1c
tavisha2 <104326277+tavisha2@users.noreply.github.com>
2023-05-21 20:17:48 -0700

    Update Main (1).java

--- Main (1).java
+++ Main (1).java
@@ -1,5 +1,6 @@
 package fls;
-
+import java.util.Timer;
+import java.util.TimerTask;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,8 +14,79 @@ import javax.swing.*;
 class Main {
   public static void main(String[] args) {
 	String[] categories = {"HISTORY","ESPN's TOP 10 ALL-TIME ATHLETES","EVERYBODY TALKS ABOUT IT...","THE COMPANY LINE","EPITAPHS & TRIBUTES","3-LETTER WORDS"};
-	String[][] questions = {{"Q1", "Q2", "Q3","Q4","Q5","Q6"},{"Q1","Q2"}};
-	String[][] answers = {{"A1","A2","A3","A4","A5","A6"},{"A1","A2","A3","A4","A5","A6"}};
+	String[][] questions = {
+		{"For the last 8 years of his life, Galileo was under house arrest for espousing this man's theory.",
+		"Built in 312 B.C. to link Rome & the South of Italy, it's still in use today",
+		"In 1000 Rajaraja I of the Cholas battled to take this Indian Ocean island now known for its tea",
+		"Karl led the first of these Marxist organizational efforts; the second one began in 1889",
+		"This Asian political party was founded in 1885 with \"Indian National\" as part of its name"},
+			
+		{"No. 2: 1912 Olympian; football star at Carlisle Indian School; 6 MLB seasons with the Reds, Giants & Braves",
+		"No. 8: 30 steals for the Birmingham Barons; 2,306 steals for the Bulls",
+		"No. 1: Lettered in hoops, football & lacrosse at Syracuse & if you think he couldn't act, ask his 11 \"unclean\" buddies",
+		"No. 10: FB/LB for Columbia U. in the 1920s; MVP for the Yankees in '27 & '36; \"Gibraltar in Cleats\"",
+		"No. 5: Only center to lead the NBA in assists; track scholarship to Kansas U.; marathoner; volleyballer"},
+		
+		{"The city of Yuma in this state has a record average of 4,055 hours of sunshine each year",
+		"In the winter of 1971-72, a record 1,122 inches of snow fell at Rainier Paradise Ranger Station in this state",
+		"On June 28, 1994 the nat'l weather service began issuing this index that rates the intensity of the sun's radiation",
+		"Africa's lowest temperature was 11 degrees below zero in 1935 at Ifrane, just south of Fez in this country",
+		"N/A"},
+		
+		{"In 1963, live on \"The Art Linkletter Show\", this company served its billionth burger",
+		"This housewares store was named for the packaging its merchandise came in & was first displayed on",
+		"This company's Accutron watch, introduced in 1960, had a guarantee of accuracy to within one minute a  month",
+		"Edward Teller & this man partnered in 1898 to sell high fashions to women",
+		"The Kirschner brothers, Don & Bill, named this ski company for themselves & the second-highest mountain"},
+		
+		{"Signer of the Dec. of Indep., framer of the Constitution of Mass., second President of the United States",
+		"\"And away we go\"",
+		"Outlaw: \"Murdered by a traitor and a coward whose name is not worthy to appear here\"",
+		"1939 Oscar winner: \"...you are a credit to your craft, your race and to your family\"",
+		"Revolutionary War hero: \"His spirit is in Vermont now\""},
+		
+		{"In the title of an Aesop fable, this insect shared billing with a grasshopper",
+		"Cows regurgitate this from the first stomach to the mouth & chew it again",
+		"A small demon, or a mischievous child (who might be a little demon!)",
+		"In geologic time one of these, shorter than an eon, is divided into periods & subdivided into epochs",
+		"A single layer of paper, or to perform one's craft diligently"}
+		
+	};
+	String[][] answers = {{"Copernicus",
+		"the Appian Way",
+		"Sri Lanka",
+		"the International",
+		"Congress Party"},
+			
+		{"Jim Thorpe",
+		"Michael Jordan",
+		"Jim Brown",
+		"Lou Gehrig",
+		"(Wilt) Chamberlain"},
+		
+		{"Arizona",
+		"Washington",
+		"UV index",
+		"Morocco",
+		"N/A"},
+		
+		{"McDonald's",
+		"Crate & Barrel",
+		"Bulova",
+		"Paul Bonwit",
+		"K2"},
+		
+		{"John Adams",
+		"Jackie Gleason",
+		"Jesse James",
+		"Hattie McDaniel",
+		"Ethan Allen"},
+		
+		{"ant",
+		"cud",
+		"imp",
+		"era",
+		"ply"}};
 	
     Frame f = new Frame();
     f.addWindowListener(new WindowAdapter() {
@@ -24,7 +96,7 @@ class Main {
     });
     f.setSize(1000,800); 
     f.setBackground(new Color(0,0,0));
-    /*
+    
     NameGetter n = new NameGetter(1);
     while (n.getName() == null){
     	n.setVisible(true);
@@ -42,49 +114,26 @@ class Main {
     n.setVisible(false);
     n2.setVisible(false);
     
-    */
-    Player current;
-    
+    Player current = p;
     
     JLabel question = new JLabel("");
-    question.setBounds(100,700,200,30);
+    question.setBounds(100,700,800,30);
     f.add(question);
     question.setForeground(new Color(255,255,255));
     
-    for(int i = 0; i < 6;i++){
-      Title t = new Title(categories[i],20 + (160*i),100);
-      f.add(t);
-    }
-    int x = 20;
-    for (int i = 0; i < 6; i++){
-      int y = 140;
-      for (int j = 200; j <= 1000; j+=200){
-        Card c = new Card(x,50 + y,""+ j,i,(j/200)-1);
-        c.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent e) {
-        		c.setBackground(new Color(100,100,100));
-        		c.setEnabled(false);
-        		question.setText(questions[c.getRow()][c.getCol()]);
-        	}
-        });
-        y+=90;
-        f.add(c);
-      }
-      x+= 160;
-    }
+    Label turn = new Label("");
+    turn.setBounds(100,650,300,30);
+    turn.setForeground(new Color(255,255,255));
+    turn.setVisible(true);
+    f.add(turn);
     
-    //help from stack overflow
-    f.addKeyListener(new KeyAdapter() {
-    	public void keyTyped(KeyEvent e) {
-    		int keycode = e.getKeyCode();
-    		if (keycode == 65) {
-    			question.setText("p111");
-    		}
-    		else if(keycode == 71) {
-    			question.setText("p2");
-    		}
-    	}
-    });
+    Label ugh = new Label("eeee");
+    ugh.setBounds(400,650,300,30);
+    ugh.setForeground(new Color(255,255,255));
+    ugh.setVisible(true);
+    f.add(ugh);
+    
+    Label selectedC = new Label();
     
     JLabel what = new JLabel("What is...");
     what.setBounds(330,750,70,20);
@@ -100,16 +149,127 @@ class Main {
     submit.setBackground(new Color(0,0,255));
     submit.setForeground(new Color(255,255,0));
    
+    
+    for(int i = 0; i < 6;i++){
+      Title a = new Title(categories[i],20 + (160*i),100);
+      f.add(a);
+    }
+    
+    int x = 20;
+    for (int i = 0; i < 6; i++){
+      int y = 140;
+      for (int j = 200; j <= 1000; j+=200){
+        Card c = new Card(x,50 + y,""+ j,(j/200)-1,i);
+        c.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent e) {
+        		what.setVisible(true);
+        		t.setVisible(true);
+        		submit.setVisible(true);
+        		c.setBackground(new Color(100,100,100));
+        		question.setText(questions[c.getCol()][c.getRow()]);
+        		turn.setText("Click now!");
+        		c.setEnabled(false);
+        		f.setFocusable(true);
+        		ugh.setText(questBox());
+        		selectedC.setText("" + c.getRow() + c.getCol());
+        	}
+        });
+        y+=90;
+        f.add(c);
+      }
+      x+= 160;
+    }
+    
+    
     submit.addActionListener(new ActionListener(){
     	public void actionPerformed(ActionEvent e) {
     		what.setVisible(false);
     		t.setVisible(false);
     		submit.setVisible(false);
+    		int r = Integer.parseInt(selectedC.getText().substring(0,1));
+    		int c = Integer.parseInt(selectedC.getText().substring(1));
+    		int money = (r+1)*200;
+    		if (t.getText().equalsIgnoreCase(answers[c][r])){
+    			question.setText("Right! Choose another question!");
+    			current.addMoney(money);
+    			
+    		}else{
+    			question.setText("Wrong! Choose another question!");
+    			current.subMoney(money);
+    		}
     	}
     });
     f.add(submit);
+    
+ 
+    
 
     f.setLayout(null);   
     f.setVisible(true); 
+  }
+  public static String questBox() {
+	  
+	  Frame f = new Frame();
+	  f.setSize(1000,800); 
+	   f.setBackground(new Color(0,0,0));
+	   //code ranch
+	  f.setFocusable(true);
+	  
+	  Label l = new Label();
+	  l.setBounds(100,650,300,30);
+	  l.setForeground(new Color(255,255,255));
+	  l.setVisible(true);
+	  f.add(l);
+	  
+	  Label whoWin = new Label("Buzz in with 'a' (player 1) and 'g' (player 2) keys! ");
+	  whoWin.setBounds(100,440,300,30);
+	  whoWin.setForeground(new Color(255,255,255));
+	  whoWin.setVisible(true);
+	  f.add(whoWin);
+	  
+	  KeyAdapter keyad = new KeyAdapter() {
+	    	
+	    	public void keyPressed(KeyEvent e) {
+	    		int keycode = e.getKeyCode();
+	    		if ((keycode == KeyEvent.VK_A)){
+	    			whoWin.setText("Player 1 wins!!");
+	    			f.dispose();
+	    		}
+	    		else if((keycode == KeyEvent.VK_G)) {
+	    			whoWin.setText("Player 2 wins!");
+	    			Timer x = new Timer();
+	    			TimerTask b = new TimerTask() {
+	    				public void run() {
+	    					cancel();
+	    					f.dispose();
+	    				}
+	    			};
+	    			x.schedule(b,3000);
+	    		
+	    		}	
+	    	}
+	    };
+	    f.addKeyListener(keyad);
+	    
+	    Timer t = new Timer();
+		TimerTask h = new TimerTask(){
+			int we = 30;
+			public void run() {
+				l.setText(we +" seconds!");
+				we--;
+				if (we == 0) {
+					l.setText("Too Late!");
+					cancel();
+					f.dispose();
+				}
+				
+			}
+		};
+		t.schedule(h,1,1000);
+	  
+	  f.setLayout(null);
+	  f.setVisible(true);
+	  return whoWin.getText();
+	  
   }
 } 
