caeebcadd71864a5675072473b45b0475f9782b5
Aubrilee <96450723+Aubrilee@users.noreply.github.com>
2023-05-25 01:51:37 -0700

    Update Card.java
    changed font of the cards

--- Card.java
+++ Card.java
@@ -1,21 +1,22 @@
-package fls;
+
 import java.awt.*;
 public class Card extends Button{
-  private int row;
-  private int col;
-  public Card(int x, int y, String val,int row, int col){
-    super(val);
-    this.row = row;
-    this.col = col;
-    this.setBounds(x,y,	150,80);
-    this.setBackground(new Color(0,0,255));
-    this.setForeground(new Color(255,255,0));
-    this.setFont(new Font("Serif",0, 13));
-  }
-  public int getRow() {
-	  return row;
-  }
-  public int getCol() {
-	  return col;
-  }
+    private int row;
+    private int col;
+    public Card(int x, int y, String val,int row, int col){
+        super(val);
+        this.row = row;
+        this.col = col;
+        this.setBounds(x,y,	150,80);
+        this.setBackground(new Color(0,0,255));
+        this.setForeground(new Color(255,255,0));
+        this.setFont(new Font("Serif",1, 20));
+
+    }
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
 }
